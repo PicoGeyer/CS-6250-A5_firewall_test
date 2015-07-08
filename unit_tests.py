9c867c8aa43dd3ba4398a8b932db746f2380aef7
Pico Geyer <picogeyer@gmail.com>
2015-07-08 23:01:44 +0200

    Added Robert Moe's changes
    * Lots of tests added
    * Neater output
    
    Thanks Robert

--- unit_tests.py
+++ unit_tests.py
@@ -2,6 +2,7 @@
 
 import time
 import re
+import random
 
 class SetupError(Exception): pass
 class TestFailure(Exception): pass
@@ -12,41 +13,195 @@ required_hosts = ['e1', 'e2', 'e3', 'w1', 'w2', 'w3']
 #Each test should clean up after itself so as not to affect the other tests
 
 def block_east_west_port_1080(mn):
-    print 'running east west test'
+    print "***********************************************************************************"
+    print "TEST: 'Block all traffic in both directions between the East and West on port 1080'"
+    print "***********************************************************************************"
     failed = False
+
+    port = 1080
+
     east = ['e1', 'e2', 'e3']
     west = ['w1', 'w2', 'w3']
+
+    # Test East to West
     for e in east:
         for w in west:
-            ehost = mn.get(e)
-            whost = mn.get(w)
-            wIP = whost.IP()
-            eIP = ehost.IP()
-            print 'Starting server on {}:{}'.format(wIP, 1080)
-            whost.sendCmd('python ../test-server.py {} 1080'.format(wIP), printPid=True)
-            time.sleep(1)
-            print 'Starting client ({}) connecting to {}:{}'.format(eIP, wIP, 1080)
-            ehost.sendCmd('python ../test-client.py {} 1080'.format(wIP), printPid=True)
-            time.sleep(1)
-            whost.sendInt()
-            ehost.sendInt()
-            wdata = whost.monitor()
-            edata = ehost.monitor()
-            #A better way to indicate that we are no longer waiting for output?
-            ehost.waiting =  whost.waiting = False
-            #print 'wdata:', wdata
-            m = re.search('received', edata)
-            if m:
+            if testconnection(mn, w, e, port):
                 failed = True
-                print '!!! Connection established from client ({}) to server ({}:{})'.format(eIP, wIP, 1080)
+                print 'FAIL: Connection established from client ({}) to server ({}) on port ({})'.format(e, w, port)
             else:
-                #print 'edata:', edata
-                #print 'wdata:', wdata
-                pass
-            #print 'edata:', edata
+                print 'PASS: Connection refused from client ({}) to server ({}) on port ({})'.format(e, w, port)
+
+    # Test West to East
+    for w in west:
+        for e in east:
+            if testconnection(mn, e, w, port):
+                failed = True
+                print 'FAIL: Connection established from client ({}) to server ({}) on port ({})'.format(w, e, port)
+            else:
+                print 'PASS: Connection refused from client ({}) to server ({}) on port ({})'.format(w, e, port)
+
     if failed:
-        raise TestFailure('Connections passed through the firewall that were supposed to be blocked')
-    
+        raise TestFailure("FAILED: 'Block all traffic in both directions between the East and West on port 1080'")
+    else:
+        print "PASSED: 'Block all traffic in both directions between the East and West on port 1080'"
+
+def block_e1_to_w1_completely(mn):
+    print "***********************************************************************************"
+    print "TEST: 'Block e1 from communicating with w1 competely in both directions'"
+    print "***********************************************************************************"
+
+    # Pick 20 random numbers between port 1024 and 10000
+    ports = random.sample(xrange(1024, 10000), 20)
+
+    failed = False
+
+    # Test East To West
+    for port in ports:
+        if testconnection(mn, 'w1', 'e1', port):
+            failed = True
+            print 'FAIL: Connection established from client ({}) to server ({}) on port ({})'.format('e1', 'w1', port)
+        else:
+            print 'PASS: Connection refused from client ({}) to server ({}) on port ({})'.format('e1', 'w1', port)
+
+    # Test West To East
+    for port in ports:
+        if testconnection(mn, 'e1', 'w1', port):
+            failed = True
+            print 'FAIL: Connection established from client ({}) to server ({}) on port ({})'.format('w1', 'e1', port)
+        else:
+            print 'PASS: Connection refused from client ({}) to server ({}) on port ({})'.format('w1', 'e1', port)
+
+    if failed:
+        raise TestFailure("FAILED 'Block e1 from communicating with w1 completely in both directions'")
+    else:
+        print "PASSED: 'Block e1 from communicating with w1 completely in both directions'"
+
+def block_e2_to_w2_over_2000(mn):
+    print "***********************************************************************************"
+    print "TEST: 'Block e2 from coummincating with w2 over ports 2000-2004 in both directions'"
+    print "***********************************************************************************"
+
+    failed = False
+
+    ports = list(xrange(2000, 2005))
+
+    # Test East To West
+    for port in ports:
+        if testconnection(mn, 'w2', 'e2', port):
+            failed = True
+            print 'FAIL: Connection established from client ({}) to server ({}) on port ({})'.format('e2', 'w2', port)
+        else:
+            print 'PASS: Connection refused from client ({}) to server ({}) on port ({})'.format('e2', 'w2', port)
+
+    # Test West To East
+    for port in ports:
+        if testconnection(mn, 'e2', 'w2', port):
+            failed = True
+            print 'FAIL: Connection established from client ({}) to server ({}) on port ({})'.format('w2', 'e2', port)
+        else:
+            print 'PASS: Connection refused from client ({}) to server ({}) on port ({})'.format('w2', 'e2', port)
+
+    if failed:
+        raise TestFailure("FAILED 'Block e2 from coummincating with w2 over ports 2000-2004 in both directions'")
+    else:
+        print "PASSED: 'Block e2 from coummincating with w2 over ports 2000-2004 in both directions'"
+
+
+def allow_traffic_within_east_west(mn):
+    print "***********************************************************************************"
+    print "TEST: 'Allow all traffice within the East or West sites to port 1080'"
+    print "***********************************************************************************"
+    failed = False
+
+    port = 1080
+
+    east = ['e1', 'e2', 'e3']
+    west = ['w1', 'w2', 'w3']
+
+    # Test East Container
+    for e in east:
+        for e1 in east:
+            if e != e1:
+                if testconnection(mn, e, e1, port):
+                    print 'PASS: Connection established from client ({}) to server ({}) on port ({})'.format(e1, e, port)
+                else:
+                    failed = True
+                    print 'FAIL: Connection could not be established from client ({}) to server ({}) on port ({})'.format(e1, e, port)
+
+    # Test West Container
+    for w in west:
+        for w1 in west:
+            if w != w1:
+                if testconnection(mn, w, w1, port):
+                    print 'PASS: Connection established from client ({}) to server ({}) on port ({})'.format(w1, w, port)
+                else:
+                    failed = True
+                    print 'FAIL: Connection could not be established from client ({}) to server ({}) on port ({})'.format(w1, w, port)
+
+    if failed:
+        raise TestFailure("FAILED: 'Block all traffic in both directions between the East and West on port 1080'")
+    else:
+        print "PASSED: 'Block all traffic in both directions between the East and West on port 1080'"
+
+def block_e3_to_w3_over_3000(mn):
+    print "***********************************************************************************"
+    print "TEST: 'Block e3 from coummincating with w3 over ports 3000-3002'"
+    print "***********************************************************************************"
+
+    failed = False
+
+    ports = list(xrange(3000, 3003))
+
+    # Test East To West
+    for port in ports:
+        if testconnection(mn, 'w3', 'e3', port):
+            failed = True
+            print 'FAIL: Connection established from client ({}) to server ({}) on port ({})'.format('e3', 'w3', port)
+        else:
+            print 'PASS: Connection refused from client ({}) to server ({}) on port ({})'.format('e3', 'w3', port)
+
+    # Test West To East
+    for port in ports:
+        if testconnection(mn, 'e3', 'w3', port):
+            print 'PASS: Connection established from client ({}) to server ({}) on port ({})'.format('w3', 'e3', port)
+        else:
+            failed = True
+            print 'FAIL: Connection refused from client ({}) to server ({}) on port ({})'.format('w3', 'e3', port)
+
+    if failed:
+        raise TestFailure("FAILED 'Block e3 from coummincating with w3 over ports 3000-3002'")
+    else:
+        print "PASSED: 'Block e3 from coummincating with w3 over ports 3000-3002'"
+
+def testconnection(mn, server, client, port):
+
+    client_host = mn.get(client)
+    server_host = mn.get(server)
+
+    client_IP = client_host.IP()
+    server_IP = server_host.IP()
+
+    print 'Starting server ({}) on {}:{}'.format(server, server_IP, port)
+    server_host.sendCmd('python test-server.py {} {}'.format(server_IP, int(port)), printPid=True)
+    time.sleep(1)
+
+    print 'Starting client ({}) connecting to {}:{}'.format(client, server_IP, port)
+    client_host.sendCmd('python test-client.py {} {}'.format(server_IP, int(port)), printPid=True)
+    time.sleep(1)
+
+    client_host.sendInt()
+    server_host.sendInt()
+
+    client_data = client_host.monitor()
+    server_data = server_host.monitor()
+
+    client_host.waiting = server_host.waiting = False
+
+    if 'received' in client_data:
+        return True
+    return False
+
 def check_hosts(mn):
     missing_host = False
     for r in required_hosts:
@@ -60,9 +215,11 @@ def check_hosts(mn):
 
 def run_tests(mn):
     #list of tests to run, edit as needed
-    tests = [
-            'block_east_west_port_1080'
-            ]
+    tests = ['block_east_west_port_1080',
+             'block_e1_to_w1_completely',
+             'allow_traffic_within_east_west',
+             'block_e2_to_w2_over_2000',
+             'block_e3_to_w3_over_3000']
     #first do a sanity check 
     check_hosts(mn)
     mn.pingAll()
@@ -70,8 +227,7 @@ def run_tests(mn):
         to = globals()[t]
         try:
             to(mn)
-            print 'Test {} passed'.format(t)
         except KeyboardInterrupt:
             raise
         except Exception as e:
-            print 'Test {} failed: {}'.format(t, str(e)) 
+            print str(e)
